341459e4fa87b0abfb5ab410bc5a321d405c7921
Kevin Delfour <kde.dekkastud10@gmail.com>
2015-04-09 08:56:38 +0000

    Add number of comments on main page for each post

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,7 @@
                 var dsq = document.createElement('script');
                 dsq.type = 'text/javascript';
                 dsq.async = true;
-                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                dsq.src = '//' + disqus_shortname + '.disqus.com/count.js';
                 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
             })();
         }
