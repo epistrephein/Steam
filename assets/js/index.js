d2cc8d254eac8e6acc53df47c5f2b352fbac7492
Kevin Delfour <kde.dekkastud10@gmail.com>
2015-04-09 08:42:45 +0000

    First cleaning and release of this theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,11 +8,45 @@
 
     $(document).ready(function(){
 
-        // On the home page, move the blog icon inside the header 
-        // for better relative/absolute positioning.
+        // Test if Disqus is avalaible        
+        if (disqus_shortname != undefined) {
+            var disqus_identifier = '{{post.id}}';
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        }
+        
+        // Test if Analytics is avalaible
+        if (google_analytics_id != undefined) {
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+            
+            ga('create', google_analytics_id, 'auto');
+            ga('send', 'pageview');
+        }
 
-        //$("#blog-logo").prependTo("#site-head-content");
-
+        // Activate smoothScroll
+        smoothScroll.init({
+            speed: 800,
+            easing: 'easeInOutCubic',
+            updateURL: false,
+            offset: 125,
+        });
+        
+        // Activate Highlighting
+        hljs.initHighlightingOnLoad();
+        
     });
 
 }(jQuery));
