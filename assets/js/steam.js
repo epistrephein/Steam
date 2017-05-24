48103209e3a4b332d1a1d2ad6030653dbf2c0b95
epistrephein <epistrephein@gmail.com>
2017-05-24 07:48:55 +0200

    Auto inject data-scroll into footnotes

--- assets/js/steam.js
+++ assets/js/steam.js
@@ -6,5 +6,16 @@ smoothScroll.init({
     offset: 125
 });
 
+// Inject data-scroll attr into footnotes
+function footnotesSmoothScroll() {
+    var sups = document.getElementsByTagName('sup');
+    var footnotes = document.getElementsByClassName('footnote');
+    for (var i = 0; i < sups.length; i++) {
+        sups[i].getElementsByTagName('a')[0].setAttribute('data-scroll', '');
+        footnotes[i].getElementsByTagName('a')[0].setAttribute('data-scroll', '');
+    }
+}
+footnotesSmoothScroll();
+
 // Highlight.js
 hljs.initHighlightingOnLoad();
