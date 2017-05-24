0359f487b5eef014d806228a9df88eb1dd4c7fdc
epistrephein <epistrephein@gmail.com>
2017-05-24 07:47:30 +0200

    Move JavaScript to file

---
+++ assets/js/steam.js
@@ -0,0 +1,10 @@
+// Smooth Scroll
+smoothScroll.init({
+    speed: 800,
+    easing: 'easeInOutCubic',
+    updateURL: false,
+    offset: 125
+});
+
+// Highlight.js
+hljs.initHighlightingOnLoad();
